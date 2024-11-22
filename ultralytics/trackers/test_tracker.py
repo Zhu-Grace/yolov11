57c9403bd63b8cdf278f06ee4a40ea7bd0a99018
grace--zz <zhujiaxuan2410@foxmail.com>
2024-11-23 00:23:43 +0800

    update

--- ultralytics/trackers/test_tracker.py
+++ ultralytics/trackers/test_tracker.py
@@ -1,13 +1,70 @@
+from collections import defaultdict
+
+import cv2
+import numpy as np
+
 from ultralytics import YOLO
 
-# Load an official or custom model
-model = YOLO("D:\zhujiaxuan\project\yolov11\models\yolo11n.pt")  # Load an official Detect model
-# model = YOLO("yolo11n-seg.pt")  # Load an official Segment model
-# model = YOLO("yolo11n-pose.pt")  # Load an official Pose model
-# model = YOLO("path/to/best.pt")  # Load a custom trained model
+# Load the YOLO11 model
+model = YOLO("yolo11n.pt")
 
-# Perform tracking with the model
-results = model(source="test.mp4", show=True)  # Tracking with default tracker
-# results = model.track(
-#     source="https://youtu.be/LNwODJXcvt4", show=True, tracker="bytetrack.yaml"
-# )  # Tracking with ByteTrack tracker
+# Open the video file
+video_path = "test.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Store the track history
+track_history = defaultdict(lambda: [])
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        print(type(frame))
+        # Check if frame is a numpy array
+        if isinstance(frame, np.ndarray):
+            # Run YOLO11 tracking on the frame, persisting tracks between frames
+            results = model.track(frame, persist=True)
+
+            # Get the boxes and track IDs
+            boxes = results[0].boxes.xywh.cpu()
+            track_ids = results[0].boxes.id.int().cpu().tolist()
+
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            # Plot the tracks
+            for box, track_id in zip(boxes, track_ids):
+                x, y, w, h = box
+                track = track_history[track_id]
+                track.append((float(x), float(y)))  # x, y center point
+                if len(track) > 30:  # retain 30 tracks for 30 frames
+                    track.pop(0)
+
+                # Draw the tracking lines
+                points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                cv2.polylines(
+                    annotated_frame,
+                    [points],
+                    isClosed=False,
+                    color=(230, 230, 230),
+                    thickness=10,
+                )
+
+            # Display the annotated frame
+            cv2.imshow("YOLO11 Tracking", annotated_frame)
+
+            # Break the loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            print("Frame is not a numpy array.")
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
